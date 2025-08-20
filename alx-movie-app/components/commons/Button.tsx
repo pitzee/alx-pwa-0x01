455523931ff8422bc86d09587e8389f5c401e7ae
pitzee <pitzee421@gmail.com>
2025-08-20 22:20:48 +0300

    error fixed

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+interface ButtonProps {
+  title: string;
+  action?: () => void | Promise<boolean>;
+}
+
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
+  const handleClick = () => {
+    if (action) {
+      action();
+    }
+  };
+
+  return (
+    <button
+      className="bg-[#E2D609] text-black px-6 py-2 rounded-md font-semibold hover:bg-[#c4c208] transition-colors duration-300"
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
